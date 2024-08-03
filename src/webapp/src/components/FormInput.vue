30cbef60a33e68aa9703e158fedbdad328b3e6db
matheusAMDS <matheusa56@gmail.com>
2024-08-03 16:40:47 -0300

    feat: Componente para Input de texto

---
+++ src/webapp/src/components/FormInput.vue
@@ -0,0 +1,25 @@
+<script lang="ts">
+export default {
+  props: {
+    value: String,
+    label: String,
+    id: String,
+    type: String,
+  },
+};
+</script>
+
+<template>
+  <div class="flex flex-col">
+    <label v-if="label" :for="id" class="font-semibold mb-1">
+      {{ label }}
+    </label>
+    <input
+      :class="'bg-muted rounded p-2'"
+      :id="id"
+      :type="type"
+      v-bind:value="value"
+      v-on:input="$emit('input', $event.target.value)"
+    />
+  </div>
+</template>

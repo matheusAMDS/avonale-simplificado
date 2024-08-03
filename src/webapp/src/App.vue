1b964a21235f3acd784fdc41874ef912ea2836bd
matheusAMDS <matheusa56@gmail.com>
2024-08-03 00:47:26 -0300

    feat: setup webapp Vue 2, TypeScript e TailwindCSS

---
+++ src/webapp/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div id="app">
+    <header>
+    </header>
+
+    <router-view />
+  </div>
+</template>
---
+++ src/webapp/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script lang="ts">
+export default {
+  data() {
+    return {
+      message: "Oiiii",
+    };
+  },
+};
+</script>
+
+<template>
+  <main>
+    <h1>Hello World</h1>
+    <p>{{ message }}</p>
+  </main>
+</template>

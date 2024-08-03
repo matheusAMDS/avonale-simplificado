d36e0ab1145947b3c84ce6af14240e6311200a79
matheusAMDS <matheusa56@gmail.com>
2024-08-03 16:40:34 -0300

    feat: Componente para botão

---
+++ src/webapp/src/components/Button.vue
@@ -0,0 +1,32 @@
+<script lang="ts">
+import type { PropType } from "vue";
+
+type ButtonVariant = "primary" | "secondary" | "basic";
+
+export default {
+  props: {
+    type: String,
+    label: {
+      type: String,
+    },
+    variant: {
+      type: String as PropType<ButtonVariant>,
+    },
+  },
+  computed: {
+    bgColor: function () {
+      return {
+        "bg-primary": this.variant == "primary",
+        "bg-secondary": this.variant == "secondary",
+        "bg-muted": this.variant == "basic",
+      };
+    },
+  },
+};
+</script>
+
+<template>
+  <button :type="type" :class="['w-full p-2 rounded', bgColor]">
+    <span class="font-medium">{{ label }}</span>
+  </button>
+</template>

b98b6cc90ee7be269fddb02cecd8b7e9a7fa877c
matheusAMDS <matheusa56@gmail.com>
2024-08-03 22:05:51 -0300

    feat: métodos para somar e subtrair valores Money

--- src/dotnet/AvonaleSimplificado.Domain/Common/Money.cs
+++ src/dotnet/AvonaleSimplificado.Domain/Common/Money.cs
@@ -1,4 +1,15 @@
 namespace AvonaleSimplificado.Domain.Common
 {
-    public record Money(double Value);
+    public record Money(double Value)
+    {
+        public static Money Sum(Money value1, Money value2)
+        {
+            return new Money(value1.Value + value2.Value);
+        }
+
+        public static Money Sub(Money value1, Money value2)
+        {
+            return new Money(value1.Value - value2.Value);
+        }
+    }
 }
